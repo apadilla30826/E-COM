3b393770f8131f52462eba7b9ba122fb4b41d404
apadilla <andrespadilla71@hotmail.com>
2025-01-08 22:54:22 -0600

    Iteración 8 de Enero - Implementación de EF

---
+++ E-COMWeb/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace E_COMWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ E-COMWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using E_COMWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_COMWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) 
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
